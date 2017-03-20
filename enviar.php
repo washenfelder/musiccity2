4d3734001dc4dfbe56004a19b3220c74e9a48d5f
Diego Beirac <diegobasspower@hotmail.com>
2017-03-20 16:25:02 -0300

    modificacion de varias secciones y creacion del formulario de contacto

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php
+
+  $nombre = $_POST['nombre'];
+  $telefono = $_POST['telefono'];
+  $mail = $_POST['email'];
+  $mensaje = $_POST['mensaje'];
+  $asuntoUsuario = $_POST['asunto'];
+
+  $cuerpo = 'Remitente: ' . $nombre . ' ' . $telefono . '<br>';
+  $cuerpo .= 'Email: ' . $mail . '<br>';
+  $cuerpo .= 'Mensaje: ' . $mensaje . '<br>';
+
+  $destino = 'musiccity@live.com.ar';
+  $asunto = $asuntoUsuario;
+
+  $encabezados = 'MIME-Version: 1.0' . "\r\n";
+  $encabezados .= 'Content-type:text/html;charset=UTF-8' . "\r\n";
+  $encabezados .= 'From: ' . $nombre . ' ' . $telefono . '<' . $mail . '>' . "\r\n"; 
+
+  mail( $destino, $asunto, $cuerpo, $encabezados );
+  header("Location: contact.html"); 
+
+?>
