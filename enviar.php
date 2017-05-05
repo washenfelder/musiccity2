2d3dcc835097896e74616d706fefd450dc487778
Diego Beirac <diegobasspower@hotmail.com>
2017-05-04 22:41:13 -0300

    formulario de contacto

--- enviar.php
+++ enviar.php
@@ -1,23 +1,12 @@
 <?php
-if(isset($_POST['submit'])){
-  $nombre = $_POST['nombre'];
-  $telefono = $_POST['telefono'];
-  $mail = $_POST['email'];
-  $mensaje = $_POST['mensaje'];
-  $asuntoUsuario = $_POST['asunto'];
+    $destino="diegobasspower@hotmail.com";
+    $nombre=$_POST["name"];
+    $email=$_POST["email"];
+    $asunto=$_POST["asunto"];
+    $mensaje=$_POST["message"];
+    $contenido="Nombre: " . $nombre . "/nEmail: " . $email . "/nAsunto: " .$asunto ."/nMensaje: " . $message;
 
-  $cuerpo = 'Remitente: ' . $nombre . ' ' . $telefono . '<br>';
-  $cuerpo .= 'Email: ' . $mail . '<br>';
-  $cuerpo .= 'Mensaje: ' . $mensaje . '<br>';
-
-  $destino = 'diegobasspower@hotmail.com';
-  $asunto = $asuntoUsuario;
-
-  $encabezados = 'MIME-Version: 1.0' . "\r\n";
-  $encabezados .= 'Content-type:text/html;charset=UTF-8' . "\r\n";
-  $encabezados .= 'From: ' . $nombre . ' ' . $telefono . '<' . $mail . '>' . "\r\n"; 
-
-  mail( $destino, $asunto, $cuerpo, $encabezados );
-  header("Location: contact.html"); 
-}
+    mail($destino,"Contacto",$contenido);
+    header("Location: index.html");
 ?>
+
